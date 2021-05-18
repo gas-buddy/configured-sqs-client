e60f27f8675204743c7de69939084df133b10d09
ergurjeet <er.gurjeet@gmail.com>
2021-05-18 11:17:33 -0600

    Test to assert compression of 1mb message

--- tests/test_compression.js
+++ tests/test_compression.js
@@ -1,6 +1,7 @@
 import tap from 'tap';
 import uuid from 'uuid/v4';
 import SqsClient from '../src/index';
+import oneKbPayload from './mock/1kb.json';
 
 const sqsHost = process.env.SQS_HOST || 'localhost';
 const sqsPort = process.env.SQS_PORT || 9324;
@@ -48,13 +49,18 @@ function getPromiseAcceptor() {
 
 tap.test('test_compression', async (t) => {
   const messageId = uuid();
+  const largeMsgId = uuid();
   const errorId = uuid();
   const sqs = new SqsClient(ctx, qConfig);
   const { accept: doneAccept, promise: receivePromise } = getPromiseAcceptor();
+  const { accept: doneAcceptLarge, promise: receivePromiseLarge } = getPromiseAcceptor();
   await sqs.subscribe(ctx, 'basic', (req, message, envelope) => {
     if (message.messageId === messageId) {
       t.strictEquals(undefined, envelope.MessageAttributes['Content-Encoding'], 'Content-Encoding attribute should not be returned as it is already resolved');
       doneAccept(true);
+    }
+    if (message.largeMsgId === largeMsgId) {
+      doneAcceptLarge(true);
     }
   });
 
@@ -78,8 +84,22 @@ tap.test('test_compression', async (t) => {
   } catch (error) {
     t.strictEquals('InvalidEncoding', error.code, 'Should attach right code to invalid compression error');
   }
+  const largeMsg = { largeMsgId, payload: new Array(1000).fill(oneKbPayload) };
+  const kbSize = Math.floor(Buffer.byteLength(JSON.stringify(largeMsg), 'utf8') / 1024);
+  try {
+    await sqs.publish(ctx, 'basic', largeMsg);
+    t.notOk(true, `Should throw when publishing large payloads of ${kbSize} kb`);
+  } catch (error) {
+    t.strictEquals('MessageTooLong', error.code, 'Should throw for large messages');
+  }
+  try {
+    await sqs.publish(ctx, 'basic', largeMsg, { compression: true });
+    t.ok(true, `Should publish large message of ${kbSize} kb with compression`);
+  } catch (error) {
+    t.notOk(true, 'The large message should have been published');
+  }
   await sqs.publish(ctx, 'basic', { messageId }, { compression: true });
   await sqs.publish(ctx, 'redrive', { errorId }, { compression: true });
-  await Promise.all([receivePromise, redrivePromise]);
+  await Promise.all([receivePromise, receivePromiseLarge, redrivePromise]);
   await sqs.stop(ctx);
 });
